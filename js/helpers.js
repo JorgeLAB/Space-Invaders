d0b00db71ca01b6a83385491a6c904c26dca3945
Jorge Borges <jorgetrabalho2019@gmail.com>
2019-09-26 07:57:02 -0300

    Syntaxe error correction in window.requestAnimationFrrame(loop, screen.canvas)

--- js/helpers.js
+++ js/helpers.js
@@ -1,20 +1,21 @@
 // script with functions and class that will support my game screen
 // Helper functions
 
-// Screen
+// Screens
 function Screen(width, height){
 	this.canvas = document.createElement("canvas");
 	this.canvas.width = this.width = width;
 	this.canvas.height = this.height = height;
-	this.ctx = this.canvas.getContext("2d");
+	this.ctx = this.canvas.getContext("2d"); /* What do getContex*/
 
 	document.body.appendChild(this.canvas);
+    };
 
-};
 Screen.prototype.drawSprite = function(sp,x, y){
     this.ctx.drawImage(sp.img, sp.x, sp.y, sp.w , sp.h, x, y, sp.w, sp.h);
 
-}; // prototype allow objects inherit methods and properties
+}; 
+// prototype allow objects inherit methods and properties
 
 // Sprite = is the grafic object that movie in the game without a trace
 function Sprite(img, x, y, w, h){
@@ -26,31 +27,32 @@ function Sprite(img, x, y, w, h){
 };
 
 
+
 // InputHandeler  
 
-function InputHandler(){
+function InputHandeler(){
    this.down = {};
    this.pressed = {};
 
-   var_this = this;
+   var _this = this;
    document.addEventListener("keydown", function(evt){
-      _this.down[evt.keyCode];
+      _this.down[evt.keyCode] = true;
 
    });
    document.addEventListener("keyup",function(evt){
-   	delete _this.down[evt.keyCode];
-   	delete _this.pressed[evt.keyCode];
+       delete _this.down[evt.keyCode];
+   	   delete _this.pressed[evt.keyCode];
    });
 };
 
-InputHandler.prototype.isDown = function(code){
+InputHandeler.prototype.isDown = function(code){
     return this.down[code];
 };
 
 InputHandeler.prototype.isPressed = function(code){
     if(this.pressed[code]){
     	return false;
-    }else kif(this.down[code]{
+    }else if(this.down[code]){
     	return this.pressed[code] = true;
     }
     return false;
